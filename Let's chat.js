7df71ef22dbb7bfec1bae3a5dad420eb287d3ef6
MahathiRiddhi <99087413+MahathiRiddhi@users.noreply.github.com>
2022-12-03 20:20:39 +0530

    Add files via upload

---
+++ Let's chat.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA_sYNn8HQeJ8jh54E7JsagwVLl1PB-OFY",
+    authDomain: "let-s-chat-dc5cc.firebaseapp.com",
+    databaseURL: "https://let-s-chat-dc5cc-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-dc5cc",
+    storageBucket: "let-s-chat-dc5cc.appspot.com",
+    messagingSenderId: "154179672227",
+    appId: "1:154179672227:web:e028c3744a82c98d0d4716",
+    measurementId: "G-FHP7RFH8EV"
+  };
+  firebase.initializeApp(firebaseConfig);
